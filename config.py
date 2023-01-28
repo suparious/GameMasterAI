c650c299dfa895cc1e45a2421986f58a03440f24
Shaun Prince <shaun@techfusion.ca>
2023-01-28 12:36:11 -0800

    RibRunBot v1.3.x initial commit

---
+++ config.py
@@ -0,0 +1,51 @@
+version = "0.3.11"
+debug = False
+screen_size = "1080p" # 1080p/1440p
+# Set keybind for Auto-Run
+autorunKey = '`'
+# distance to move foward without finding something
+fowardMoveTotal = 45
+forwardMoveKey = 'w'
+reverseMoveKey = 's'
+# Go right or left
+flip = 1
+# Turn 90 degrees, value will be different for you, im on a 4k monitor
+flipMouseMove = 900
+gameTitle = "New World"
+maxStuck = 10
+actionKey = 'e'
+# The keybind for selecting the active weapon (currently needs to be set to 1, with your secondary as 2)
+weaponSelect1 = '1'
+weaponSelect2 = '2'
+weaponSelectDelay = 0.5
+# The keybinds for your active combat skills
+combatKey1 = 'q'
+combatKey1delay1 = 1
+combatKey1delay2 = 2
+combatKey2 = 'r'
+combatKey2delay1 = 1
+combatKey2delay2 = 1
+combatKey3 = 'f'
+combatKey3delay1 = 3
+combatKey3delay2 = 3
+# Use health potions during combat
+takePots = True
+hotbarKey1 = '3'
+# Proficiency Pot Key
+hotbarKey2 = '4'
+# Food Buff Key
+hotbarKey3 = '5'
+# Trade skill food
+hotbarKey4 = '6'
+# How many times we can get fucked until we try to teleport out
+fuckedMax = 5
+# Number of loops before checking the bag space
+bagCheckDelayMax = 75 # 75
+# weapon activate / deactivate
+weaponActivate = 'x'
+prone = 'z'
+crouch = 'c'
+# list of usable emotes
+emote_list = ["thumbs", "rockpaperscissors", "dice", "cheer", "flex"]
+images_folder = "imgs"
+runtime_images_folder = "debug"
